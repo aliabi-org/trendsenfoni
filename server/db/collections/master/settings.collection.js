1299177caac30c8f449f27efa3965dc9d987d958
Ali TEK <alitek@gmail.com>
2024-10-08 13:44:59 +0300

    updated

--- server/db/collections/master/settings.collection.js
+++ server/db/collections/master/settings.collection.js
@@ -6,7 +6,24 @@ module.exports = function (dbModel) {
 			connector: {
 				clientId: { type: String, default: '', index: true },
 				clientPass: { type: String, default: '', index: true },
+				connectionType: { type: String, enum: ['mssql', 'mysql', 'pg', 'fs', 'excel'], default: 'mssql', index: true },
+				mssql: {
+					user: { type: String, default: 'sa' },
+					password: { type: String, default: '' },
+					database: { type: String, default: '' },
+					server: { type: String, default: '' },
+					port: { type: Number, default: 1433 },
+					dialect: { type: String, default: 'mssql' },
+					dialectOptions: {
+						instanceName: { type: String, default: 'SQLExpress' }
+					},
+					options: {
+						encrypt: { type: Boolean, default: false },
+						trustServerCertificate: { type: Boolean, default: true },
+					}
+				}
 			},
+
 		},
 		{ versionKey: false, timestamps: true }
 	)
